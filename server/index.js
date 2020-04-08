e9b76af9f24d32a9f5f5e31874cb795d710bef9e
jvenci <jvenci@comcast.net>
2020-04-08 03:37:39 -0700

    Implement basic socket

--- server/index.js
+++ server/index.js
@@ -10,6 +10,13 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+  console.log('A new connection has formed');
+
+  socket.on('disconnect', () => {
+    console.log('User has terminated connection');
+  })
+})
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server initialized on port ${PORT}`));
