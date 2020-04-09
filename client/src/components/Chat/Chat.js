a2b33cbe6b2468b919a6c0df2a702786bb884548
Julian V <jvenci@comcast.net>
2020-04-09 04:11:04 -0700

    Implement useEffect for message and sendMessage function

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+import './Chat.css';
+
 let socket;
 
 const Chat = ( { location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
   
   const ENDPOINT = 'localhost:5000';
 
@@ -28,8 +32,29 @@ const Chat = ( { location }) => {
   
   }, [ENDPOINT, location.search]);
 
+  useEffect( () => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message]);
+    })
+  }, [messages]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''))
+    }
+  }
+
+  console.log(message, messages)
+
   return (
-    <h1>Chat</h1>
+    <div className="outerContainer">
+      <div className="container">
+        <input value={message} 
+        onChange={e => setMessage(e.target.value)}
+        onKeyPress={e => e.key === 'Enter' ? sendMessage(e) : null}></input>
+      </div>
+    </div>
   )
 }
 
